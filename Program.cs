2c191b821472ec89dc8f96199b51ac7fb0e37035
Axel Giovannai <axel.giovannai@proton.me>
2026-04-19 18:57:04 +0200

    oupsi ! ^^

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddDbContext<TaskFlowDbContext>(options =>
 // Services
 builder.Services.AddScoped<IJwtService, JwtService>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<ITaskService, TaskService>();
 
 // JWT configuration
 var jwtSettings = builder.Configuration.GetSection("Jwt");
